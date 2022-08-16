d9a3c0fd364011a00c78b0200b4f68cb71dac03f
DavDaz <ddiazlink08@gmail.com>
2022-08-16 16:05:00 -0500

    Programa1 terminado

---
+++ peso-en-otro-planeta/operador.js
@@ -0,0 +1,20 @@
+
+
+
+weight =  prompt("Ingresa tu peso en Kg: ");
+
+
+weightResult = (_martGravity * weight)/_eartGravity;
+document.write("Tu peso en Martes seria: " + weightResult + "kg");
+
+
+/*if (select == 1) {
+    weightResult = "Tu peso en Marte es: "+ (_martGravity * weight)/_eartGravity +"Kg";
+} else if (select == 2){
+    weightResult = "Tu peso en Jupiter es: "+ (_jupiterGravity * weight)/_eartGravity +"Kg";
+} else {
+    weightResult = "Error: Seleccion invalida";
+}
+*/
+
+
